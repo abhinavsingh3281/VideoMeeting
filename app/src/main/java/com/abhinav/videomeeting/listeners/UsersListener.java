06f670a30750d91ba31757e13b28affb78d8bafc
Abhinav <abhinavhissar@gmail.com>
2021-12-13 23:54:26 +0530

    Initial commit

---
+++ app/src/main/java/com/abhinav/videomeeting/listeners/UsersListener.java
@@ -0,0 +1,12 @@
+package com.abhinav.videomeeting.listeners;
+
+import com.abhinav.videomeeting.models.User;
+
+public interface UsersListener {
+
+    void initiateVideoMeeting(User user);
+
+    void initiateAudioMeeting(User user);
+
+    void onMultipleUsersAction(Boolean isMultipleUsersSelected);
+}
---
+++ app/src/main/java/com/abhinav/videomeeting/models/User.java
@@ -0,0 +1,7 @@
+package com.abhinav.videomeeting.models;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    public String firstName, lastName, email, token;
+}
